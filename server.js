ae59ce35d17fc58f411a6277cc346c94819c9426
malveira_gbr <gabrielmalveira01@gmail.com>
2024-12-05 17:25:15 -0300

    incluindo rotas

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 require('dotenv').config();
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
 const MongoStore = require('connect-mongo');
 const session = require('express-session');
-const path = require('path');
 const flash = require('connect-flash');
-const app = express();
-const port = process.env.PORT||3006;
-
+const path = require('path')
+const port = process.env.PORT||3006
+const routes = require('./routes')
 
 //connectDB
-
 async function connectDB() {
     try{
-
+        await mongoose.connect(process.env.CONNECTIONSTRING)
+        console.log('conectado á base de dados')
+        app.emit('pronto')
     }catch(err){
-        console.error('erro ao conectar ao Mongo:', err)
+        console.error('erro ao conectar á base de dados', err)
+        app.emit('erro')
     }
-}
+} 
 
+
+connectDB()
+
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+
+app.use(routes)
 app.listen(port, () =>{
-    console.log(`Servidor rodando na porta ${port}` )
-})
+    console.log(`Servidor rodando na porta ${port}`)
+});
